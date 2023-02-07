fbb20ac03967650a22246e8dc1dd749191419ef9
yangddoddi <pud1251@gmail.com>
2023-02-07 22:42:27 +0900

    jenkins ci test

--- src/main/java/com/example/momobe/MomoBeApplication.java
+++ src/main/java/com/example/momobe/MomoBeApplication.java
@@ -2,6 +2,7 @@ package com.example.momobe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-public class MomoBeApplication {
+public class MomoBeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MomoBeApplication.class, args);
